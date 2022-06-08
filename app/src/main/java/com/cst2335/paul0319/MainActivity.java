08a4f748debb24914fc1ca4f570f80e579f48e32
JayPaulinCodes <jacobwpaulin@gmail.com>
2022-06-08 18:14:50 -0400

    Finished Lab 3

--- app/src/main/java/com/cst2335/paul0319/MainActivity.java
+++ app/src/main/java/com/cst2335/paul0319/MainActivity.java
@@ -3,34 +3,82 @@ package com.cst2335.paul0319;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static String INTENT_EMAIL = "Intent_Email";
 
+    private final static String ACTIVITY_NAME = "MAIN ACTIVITY";
+    private final static String PREFERENCES_FILE = "MyData";
+    private final static String PREFERENCE_RESERVED_EMAIL = "Reserve_Email";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        log("In onCreate, creating the objects");
         setContentView(R.layout.activity_main);
+
+        log("In onCreate, setting element variables");
+        EditText emailInput = findViewById(R.id.input_email);
+        Button loginButton = findViewById(R.id.login_button);
+
+        log("In onCreate, getting shared preferences");
+        SharedPreferences sharedPreference = getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor sharedPreference_Editor = sharedPreference.edit();
+
+        log("In onCreate, getting shared preferences values");
+        String saved_email = sharedPreference.getString(PREFERENCE_RESERVED_EMAIL, "");
+
+        log("In onCreate, setting inputs from saved values");
+        emailInput.setText(saved_email);
+
+        log("In onCreate, setting button events");
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                savePreferences();
+
+                Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+                goToProfile.putExtra(INTENT_EMAIL, emailInput.getText().toString());
+            }
+        });
 
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        log("In onPause, beginning");
 
-        SharedPreferences sharedPreference = getSharedPreferences(getString(R.string.sharedPreference), Context.MODE_PRIVATE);
+        log("In onPause, saving preferences");
+        savePreferences();
+    }
+
+    private void log(String message) { Log.i(ACTIVITY_NAME, message); }
+
+    private void savePreferences() {
+        log("In Method savePreferences, setting input variables");
+        EditText emailInput = findViewById(R.id.input_email);
+
+        log("In Method savePreferences, getting shared preferences");
+        SharedPreferences sharedPreference = getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
         SharedPreferences.Editor sharedPreference_Editor = sharedPreference.edit();
 
         // Put Email
-        sharedPreference_Editor.putString(getString(R.string.sharedPreference_Email), email);
-
-        // Put Password
-        sharedPreference_Editor.putString(getString(R.string.sharedPreference_Password), password);
+        log("In Method savePreferences, storing values");
+        sharedPreference_Editor.putString(PREFERENCE_RESERVED_EMAIL, emailInput.getText().toString());
 
         // Save Changes
+        log("In Method savePreferences, applying changes");
         sharedPreference_Editor.apply();
     }
 }
---
+++ app/src/main/java/com/cst2335/paul0319/ProfileActivity.java
@@ -0,0 +1,100 @@
+package com.cst2335.paul0319;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private final static String ACTIVITY_NAME = "PROFILE ACTIVITY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        log("In onCreate, creating the objects");
+        setContentView(R.layout.activity_profile);
+
+        log("In onCreate, getting passed intent values");
+        Intent fromMain = getIntent();
+
+        log("In onCreate, setting element variables");
+        ImageButton image_button = findViewById(R.id.input_5_picture);
+        EditText emailInput = findViewById(R.id.input_4_email);
+
+        log("In onCreate, setting email text based off of the intent passed values");
+        emailInput.setText(fromMain.getStringExtra(MainActivity.INTENT_EMAIL));
+
+        ActivityResultLauncher<Intent> myPictureTakerLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult()
+            ,new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Bitmap imgbitmap = (Bitmap) data.getExtras().get("data");
+                        image_button.setImageBitmap(imgbitmap); // the imageButton
+                    }
+                    else if(result.getResultCode() == Activity.RESULT_CANCELED)
+                        log("User refused to capture a picture.");
+                }
+            } );
+
+
+        image_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    myPictureTakerLauncher.launch(takePictureIntent);
+                }
+            }
+        });
+
+    }
+
+    @Override //screen is visible but not responding
+    protected void onStart() {
+        super.onStart();
+        log("In onStart, visible but not responding");
+    }
+
+    @Override //screen is visible but not responding
+    protected void onResume() {
+        super.onResume();
+        log("In onResume");
+    }
+
+    @Override //screen is visible but not responding
+    protected void onPause() {
+        super.onPause();
+        log("In onPause");
+    }
+
+    @Override //not visible
+    protected void onStop() {
+        super.onStop();
+        log("In onStop");
+    }
+
+    @Override  //garbage collected
+    protected void onDestroy() {
+        super.onDestroy();
+        log("In onDestroy");
+    }
+
+    private void log(String message) { Log.i(ACTIVITY_NAME, message); }
+}
